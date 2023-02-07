0488796872e00dd73e51270c0b10683dca32258b
Peng Guo <errrrrricg@gmail.com>
2023-02-07 11:07:34 -0500

    basic cam movement

--- Assets/Scripts/Camera/CameraManager.cs
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.PlayerLoop;
 
 public class CameraManager : MonoBehaviour
 {
@@ -16,5 +17,20 @@ public class CameraManager : MonoBehaviour
             }
             return instance;
         }
+    }
+
+    private void Update()
+    {
+        transform.position += new Vector3(GetKeyboardInput().x, 0f, GetKeyboardInput().y) * Time.deltaTime * 10;
+    }
+
+    Vector2Int GetKeyboardInput()
+    {
+        Vector2Int ip = new Vector2Int(0,0);
+        if (Input.GetKey(KeyCode.W)) ip.y += 1;
+        if (Input.GetKey(KeyCode.S)) ip.y -= 1;
+        if (Input.GetKey(KeyCode.A)) ip.x -= 1;
+        if (Input.GetKey(KeyCode.D)) ip.x += 1;
+        return ip;
     }
 }
