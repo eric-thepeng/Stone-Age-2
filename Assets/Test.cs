399dccd85a6e4dd34a93e2f0b91db96c2ff3fe3e
eric-thepeng <errrrrricg@gmail.com>
2023-02-22 16:22:18 -0500

    bug fix with asset import

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
+
+[ExecuteInEditMode]
 public class Test : MonoBehaviour
 {
     private void Start()
     {
+        print("carryover sprites");
         foreach(Transform child in transform)
         {
-            print(child.name);
-            child.GetComponent<Tetris>().itemSO.tetrisSprite = child.GetComponent<SpriteRenderer>().sprite;
-            child.GetComponent<Tetris>().itemSO.iconSprite = child.GetComponent<SpriteRenderer>().sprite;
-            //child.GetComponent<Tetris>().itemSO;
+            ItemScriptableObject iso = child.GetComponent<Tetris>().itemSO;
+            iso.tetrisSprite = child.GetComponent<SpriteRenderer>().sprite;
+            iso.iconSprite = child.GetComponent<SpriteRenderer>().sprite;
+            EditorUtility.SetDirty(iso);
         }
+        AssetDatabase.SaveAssets();
     }
 }
