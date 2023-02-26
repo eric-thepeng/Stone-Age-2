1b00b7fc4efc1dcdce466269353e0aca96e41055
Mingzhi Xu <mx2091@nyu.edu>
2023-02-26 18:30:58 -0500

    Quick fix, myPrefab missing bug fix

--- Assets/Scripts/Recipe Map Panel/TempSO/RecipeListScriptableObject.cs
+++ Assets/Scripts/Recipe Map Panel/TempSO/RecipeListScriptableObject.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ItemListScriptableObject", menuName = "ScriptableObjects/ItemListScriptableObject")]
+[CreateAssetMenu(fileName = "RecipeListScriptableObject", menuName = "ScriptableObjects/RecipeListScriptableObject")]
 public class RecipeListScriptableObject : SerializedScriptableObject
 {
     public List<ItemCraftScriptableObject> list = new List<ItemCraftScriptableObject>();
