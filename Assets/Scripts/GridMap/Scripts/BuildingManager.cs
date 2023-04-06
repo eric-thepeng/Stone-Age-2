58b823f14e5f88be9cf1aaf369b47f418a951b34
eric-thepeng <errrrrricg@gmail.com>
2023-04-06 13:02:21 -0400

    folder restructure

--- Assets/Scripts/GridMap/Scripts/BuildingManager.cs
+++ Assets/Scripts/GridMap/Scripts/BuildingManager.cs
@@ -48,6 +48,7 @@ public class BuildingManager : MonoBehaviour
 
     public BuildingISO GetSelectedBuildingISO()
     {
+        if (selectedUIIB == null) return null;
         ItemScriptableObject returnISO = selectedUIIB.GetISO();
         return returnISO is BuildingISO ? (BuildingISO)returnISO :null;
     }
