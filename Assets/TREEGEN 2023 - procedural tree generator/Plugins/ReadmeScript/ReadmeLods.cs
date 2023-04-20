a9c1e350380adb4b2f0f719cd84f91fcd1830be2
Mingzhi Xu <mx2091@nyu.edu>
2023-04-20 13:08:57 -0400

    TreeGen / BP viewer and exp info board

---
+++ Assets/TREEGEN 2023 - procedural tree generator/Plugins/ReadmeScript/ReadmeLods.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ReadmeLods : MonoBehaviour
+{
+    [TextArea(3, 10)]
+    public string description = "Add your description here.";
+
+    private void OnDrawGizmosSelected()
+    {
+        UnityEditor.Handles.Label(transform.position, description);
+    }
+}
