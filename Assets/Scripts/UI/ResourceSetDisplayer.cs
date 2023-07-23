6fa41578712ae1243c3a91f4752fb89deb26aee7
eric-thepeng <errrrrricg@gmail.com>
2023-07-23 16:56:27 -0400

    finish revising resourcesetdisplayer

--- Assets/Scripts/UI/ResourceSetDisplayer.cs
+++ Assets/Scripts/UI/ResourceSetDisplayer.cs
@@ -11,16 +11,19 @@ using TMPro;
 /// </summary>
 public class ResourceSetDisplayer : MonoBehaviour
 {
+    [Header("------EDIT WHAT TO DISPLAY------")]
     [Header("Display a preset ResourceSet")]
     [SerializeField,  Tooltip("Resource set to display")] ResourceSet resourceSetToDisplay = null;
 
     [Header("OR Display a ResourceSet from IResourceSetProvider")] 
     [SerializeField] private MonoBehaviour resourceSetProvider = null;
     [SerializeField] private int resourceSetProviderIndex = 0;
+
     
-    [Header("------EDIT BELOW------")]
+    [Header("------EDIT VARIABLES------")]
     [SerializeField, Tooltip("Does it display the spirit points and amount section?")] private bool displaySpiritPoints = true;
     [SerializeField, Tooltip("Does it display the resource and amount section?")] private bool displayResource = true;
+    [SerializeField] private bool displaySign = true;
     [SerializeField, Tooltip("Does it display shadow under sprites and texts for better readability")] private bool displayShadow = true;
     [SerializeField,  Tooltip("Displacement between each set of resource+text")] Vector3 displacement = new Vector3(2,0,0);
     
@@ -37,18 +40,25 @@ public class ResourceSetDisplayer : MonoBehaviour
     //local vairables:
     private ResourceSet displayingResourceSet;
     Vector3 shadowDisplacement = new Vector3(0.04f, -0.04f, -0.04f);
-
-    private void Start()
+    
+    private void OnEnable()
     {
-        spriteAmountSetTemplate.SetActive(false);
+        Generate();
+    }
+
+    public void Generate()
+    {
         if (resourceSetToDisplay == null)
         {
             if (resourceSetProvider is IResourceSetProvider)
             {
                 resourceSetToDisplay = ((IResourceSetProvider)resourceSetProvider).ProvideResourceSet(resourceSetProviderIndex);
             }
+            else
+            {
+                Debug.LogError("ResourceSetDisplay has nothing to display");
+            }
         }
-
         Display(resourceSetToDisplay);
     }
 
@@ -59,44 +69,40 @@ public class ResourceSetDisplayer : MonoBehaviour
             Debug.LogError("No resource set to be displayed.");
             return;
         }
-        
-        if(!displaySpiritPoints)spiritPointDisplay.gameObject.SetActive(false);
-        if(!displayResource)container.gameObject.SetActive(false);
+
+        //Set up what will be displayed
+        if (displaySpiritPoints)
+        {
+            spiritPointDisplay.gameObject.SetActive(true);
+        }
+        else
+        {
+            spiritPointDisplay.gameObject.SetActive(false);
+        }
+        if (displayResource)
+        {
+            container.gameObject.SetActive(true);
+        }
+        else
+        {
+            container.gameObject.SetActive(false);
+        }
         
         if (displayingResourceSet == rs)
         {
-            print("no need to recalculate");
-            return;
-        }
+            //print("no need to recalculate");
+            //return;
+        } 
         ClearDisplay();
         displayingResourceSet = rs;
+        spriteAmountSetTemplate.SetActive(true);
         GenerateIcons();
+        GenerateSpiritPoints();
+        spriteAmountSetTemplate.SetActive(false);
     }
     
     private void GenerateIcons()
     {
-        //Generate Spirit Point
-        if (displaySpiritPoints)
-        {
-            //Set up spirit point amount
-            SpriteRenderer sr = spiritPointDisplay.GetComponentInChildren<SpriteRenderer>();
-            TextMeshPro tmp = spiritPointDisplay.GetComponentInChildren<TextMeshPro>();
-            tmp.text = ""+displayingResourceSet.spiritPoint;
-            
-            if (displayShadow) //generate shadow
-            {
-                SpriteRenderer srShadow = Instantiate(sr.gameObject, shadowsContainer).GetComponent<SpriteRenderer>();
-                srShadow.color = Color.black;
-                srShadow.transform.localPosition += shadowDisplacement;
-                
-                TextMeshPro tmpShadow = Instantiate(tmp.gameObject, shadowsContainer).GetComponent<TextMeshPro>();
-                tmpShadow.color = Color.black;
-                tmpShadow.transform.localPosition += shadowDisplacement;
-
-            }
-        }
-
-        // Generate Resource
         if(!displayResource) return;
         for (int i = 0; i < displayingResourceSet.resources.Count; i++) //Generate each resource and amount
         {
@@ -104,12 +110,23 @@ public class ResourceSetDisplayer : MonoBehaviour
             ResourceSet.ResourceAmount ra = displayingResourceSet.resources[i];
             GameObject go = Instantiate(spriteAmountSetTemplate, container);
             go.SetActive(true);
-            SpriteRenderer sr = go.GetComponentInChildren<SpriteRenderer>();
-            TextMeshPro tmp = go.GetComponentInChildren<TextMeshPro>();
+            SpriteRenderer sr = go.transform.Find("Sprite").GetComponent<SpriteRenderer>();
+            TextMeshPro tmp = go.transform.Find("Amount").GetComponent<TextMeshPro>();
+            TextMeshPro sign = go.transform.Find("Sign").GetComponent<TextMeshPro>();
             sr.sprite = ra.iso.iconSprite;
             tmp.text = "" + ra.amount;
+            if (displaySign)
+            {
+                sign.gameObject.SetActive(true);
+                sign.text = ra.amount >= 0 ? "+" : "-"; 
+            }
+            else
+            {
+                sign.gameObject.SetActive(false);
+            }
             
             //generate shadow
+            /*
             if (displayShadow)
             {
                 SpriteRenderer srShadow = Instantiate(sr.gameObject, shadowsContainer).GetComponent<SpriteRenderer>();
@@ -120,7 +137,7 @@ public class ResourceSetDisplayer : MonoBehaviour
                 tmpShadow.color = Color.black;
                 tmpShadow.transform.localPosition += shadowDisplacement;
 
-            }
+            }*/
             
             //set position according to alignment
             if (alighment == Alighment.Left)
@@ -135,23 +152,47 @@ public class ResourceSetDisplayer : MonoBehaviour
             {
                 if (displayingResourceSet.resources.Count % 2 == 1) //amount display is odd
                 {
-                    int sign = (int)Mathf.Pow(-1, i % 2);
-                    go.transform.position += sign * displacement * ((i + 1) / 2);
+                    int positivity = (int)Mathf.Pow(-1, i % 2);
+                    go.transform.position += positivity * displacement * ((i + 1) / 2);
                 }
                 else //amount display is even
                 {
-                    int sign = (int)Mathf.Pow(-1, i % 2);
-                    go.transform.position += sign * displacement * (0.5f + (i / 2));
+                    int positivity = (int)Mathf.Pow(-1, i % 2);
+                    go.transform.position += positivity * displacement * (0.5f + (i / 2));
                 }
             }
         }
     }
 
+    private void GenerateSpiritPoints()
+    {
+        if(!displaySpiritPoints)return;
+        SpriteRenderer sr = spiritPointDisplay.GetComponentInChildren<SpriteRenderer>();
+        TextMeshPro tmp = spiritPointDisplay.GetComponentInChildren<TextMeshPro>();
+        tmp.text = ""+displayingResourceSet.spiritPoint;
+            
+        /*
+        if (displayShadow) //generate shadow
+        {
+            SpriteRenderer srShadow = Instantiate(sr.gameObject, shadowsContainer).GetComponent<SpriteRenderer>();
+            srShadow.color = Color.black;
+            srShadow.transform.localPosition += shadowDisplacement;
+            
+            TextMeshPro tmpShadow = Instantiate(tmp.gameObject, shadowsContainer).GetComponent<TextMeshPro>();
+            tmpShadow.color = Color.black;
+            tmpShadow.transform.localPosition += shadowDisplacement;
+        }*/
+    }
+    
     private void ClearDisplay()
     {
         for(int i = container.childCount-1; i>= 0; i--)
         {
-            Destroy(container.GetChild(i).gameObject);
+#if UNITY_EDITOR
+            DestroyImmediate(container.GetChild(i).gameObject);
+#else
+        Destroy(container.GetChild(i).gameObject);
+#endif
         }
     }
 
