0cfa40ce2680ee08846a6022e1542fdb3fb82e4a
Peng Guo M <errrrrricg@gmail.com>
2023-07-27 22:59:55 -0400

    Tetris building tool: edges

--- Assets/Editor/Internal Tools/TetrisBuildingToolEditor.cs
+++ Assets/Editor/Internal Tools/TetrisBuildingToolEditor.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using PlasticGui;
+using Unity.Mathematics;
 using UnityEditor;
 using UnityEngine;
 
 [CustomEditor(typeof(TetrisBuildingTool))]
 public class TetrisBuildingToolEditor : Editor
 {
+    List<Vector2Int> directions = new List<Vector2Int> { Vector2Int.down, Vector2Int.left, Vector2Int.up, Vector2Int.right};
     public override void OnInspectorGUI()
     {
         if (GUILayout.Button("Build All"))
@@ -27,15 +29,24 @@ public class TetrisBuildingToolEditor : Editor
             // make new game object
             GameObject newGameObject = new GameObject("Tetris_"+iso.name);
 
-            // add the unit tiles first
+            // add the unit tiles and edges
             foreach (Vector2Int coord in iso.HomogeneousCoord)
             {
-                GameObject newTetris = Instantiate(builder.unitGameObject, new Vector3(coord.x * builder.unitLength, -coord.y * builder.unitLength, 0), Quaternion.identity);
-                newTetris.transform.parent = newGameObject.transform;
+                //add unit tiles
+                Vector3 unitPosition = new Vector3(coord.x * builder.unitLength, -coord.y * builder.unitLength, 0);
+                GameObject newUnit = Instantiate(builder.unitGameObject, unitPosition, Quaternion.identity);
+                newUnit.transform.parent = newGameObject.transform;
+                
+                //add edges
+                foreach (Vector2Int dir in directions)
+                {
+                    if(iso.HomogeneousCoord.Contains(coord + dir)) continue;
+                    Vector3 edgeDeltaPosition = 0.5f * new Vector3(dir.x * builder.unitLength, -dir.y * builder.unitLength, 0);
+                    GameObject newEdge = Instantiate(builder.edgeGameObject,unitPosition + edgeDeltaPosition, quaternion.identity);
+                    newEdge.transform.parent = newGameObject.transform;
+                }
             }
 
-            // then add the edges
-        
             // save as prefab
             string localPath = builder.folderPath + "/" + newGameObject.name + ".prefab";
             localPath = AssetDatabase.GenerateUniqueAssetPath(localPath);
