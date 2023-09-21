01fd6379f1394f3f9d7ce22aea18a3693ef5a6e7
Peng Guo M <errrrrricg@gmail.com>
2023-09-21 13:19:03 -0400

    workshop isoiconbox make into prefab

--- Assets/Scripts/UI/ISO Display Box/UI_ISOIconDisplayBox.cs
+++ Assets/Scripts/UI/ISO Display Box/UI_ISOIconDisplayBox.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
@@ -10,6 +11,7 @@ public class UI_ISOIconDisplayBox : MonoBehaviour
     [SerializeField] private int index;
     [SerializeField] private bool receiveDrop = true;
     [SerializeField] private bool clickToCancel = true;
+    [SerializeField] private GameObject textAmount;
     
     public MonoBehaviour monoBehaviourWithIISOReceiver;
     
@@ -18,9 +20,18 @@ public class UI_ISOIconDisplayBox : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
     }
 
-    public void Display(ItemScriptableObject iso, bool triggerByDroppingISOIcon)
+    public void Display(ItemScriptableObject iso, bool triggerByDroppingISOIcon, int amount = -1)
     {
         if(!receiveDrop && triggerByDroppingISOIcon) return;
+        if (amount == -1)
+        {
+            textAmount.SetActive(false);
+        }
+        else
+        {
+            textAmount.SetActive(true);
+            textAmount.GetComponent<TextMeshPro>().text = "" + amount;
+        }
         if (iso == null)
         {
             Clear();
