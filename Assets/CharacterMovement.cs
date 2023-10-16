1135932da8087af2f25711754df6ec7a141ca275
MarioYangNYU <yh3919@nyu.edu>
2023-10-15 23:36:04 -0400

    Live2D小人人

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,61 @@
+using Live2D.Cubism.Core;
+using Live2D.Cubism.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+
+    private Transform _visual;
+    private CubismModel _model;
+    private Animator _animator;
+    [SerializeField] private CubismParameter _leafShadow;
+    Vector3 _originalScale;
+
+    // 为了保证prewalk不会在换方向时再次触发
+    Vector3 _previousMovement;
+
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _model = GetComponentInChildren<CubismModel>();
+        _visual = _model.transform;
+        _originalScale = _visual.transform.localScale;
+        _leafShadow = _model.Parameters[8];
+    }
+
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+
+        if (movement == Vector3.zero && _previousMovement == Vector3.zero)
+        {
+            _animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            _animator.SetBool("isWalking", true);
+        }
+
+        transform.position = transform.position + movement * Time.deltaTime * moveSpeed;
+
+        if (movement.x < 0)
+        {
+            _leafShadow.Value = 1;
+            Debug.Log(_leafShadow.Value);
+            _visual.transform.localScale = new Vector3(-_originalScale.x, _originalScale.y, _originalScale.z);
+        }
+        else if (movement.x > 0)
+        {
+            _leafShadow.Value = 0;
+            Debug.Log(_leafShadow.Value);
+            _visual.transform.localScale = new Vector3(_originalScale.x, _originalScale.y, _originalScale.z);
+        }
+
+        _previousMovement = movement;
+    }
+
+
+}
