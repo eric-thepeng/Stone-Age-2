5f039f94db52c1ea22f38b7b07cb3e6844082234
Peng Guo M <errrrrricg@gmail.com>
2023-10-19 14:54:12 -0400

    trigger quest condition, quest dialogue, quest act

--- Assets/Scripts/Quest/UniQuest/UniQuest.cs
+++ Assets/Scripts/Quest/UniQuest/UniQuest.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using Cinemachine;
 using UnityEngine;
 
@@ -10,34 +11,31 @@ public class UniQuest : MonoBehaviour
 
     [Serializable]public class TriggerQuestCondition
     {
+        [Header("DO NOT EDIT")]
+        public bool hasBeenTrigged = false;
+        [Header("Edit Conditions")]
         public bool uponGameStart = false;
+        public bool duringTutorial = false;
         public BLDExploreSpot uponDiscoverExploreSpot = null;
         public BLDExploreSpot uponUnlockExploreSpot = null;
         public BLDExploreSpot uponStartGatherExploreSpot = null;
-        public ItemScriptableObject uponFirstObtainISO = null;
-        public BuildingISO uponFirstBuildBISO = null;
-
+        public ItemScriptableObject uponObtainISO = null;
+        public int uponObtainISOAmount = 0;
+        public BuildingISO uponBuildBISO = null;
+        public int uponBuildBISOAmount = 0;
+        
         public bool isTriggered()
         {
+            if (hasBeenTrigged) return false;
+            //determin if is has been triggered, remember to change hasBeenTriggered.
             return false;
         }
     }
     
-    [Serializable]
-    public class QuestProgressAction
-    {
-        public virtual void BeginAction(){}
-    }
-    
-    [Serializable] public class NarrativeDialogue : QuestProgressAction
+    [Serializable] public class QuestDialogue
     {
         public List<string> allLines;
         public int currentLineIndex = 0;
-
-        public override void BeginAction()
-        {
-            base.BeginAction();
-        }
 
         public string GetCurrentLine()
         {
@@ -51,9 +49,47 @@ public class UniQuest : MonoBehaviour
         }
     }
 
+    [Serializable]
+    public class QuestAction
+    {
+        [Serializable]public class GameObjectAction
+        {
+            public enum ActionType{LocalMoveAmount, GlobalMoveAmount, Enable, Disable}
+
+            public ActionType actionType = ActionType.LocalMoveAmount;
+            public GameObject targetGameObject;
+            public Vector3 targetVector;
+
+            public void PerformAction()
+            {
+                if (actionType == ActionType.LocalMoveAmount) targetGameObject.transform.localPosition += targetVector;
+                else if (actionType == ActionType.LocalMoveAmount) targetGameObject.transform.position += targetVector;
+                else if (actionType == ActionType.Enable) targetGameObject.SetActive(true);
+                else if (actionType == ActionType.Disable) targetGameObject.SetActive(false);
+            }
+        }
+
+        public List<GameObjectAction> gameObjectActions;
+
+        public void PerformActions()
+        {
+            foreach (GameObjectAction goa in gameObjectActions)
+            {
+                goa.PerformAction();
+            }
+        }
+    }
+
     #endregion
 
-
     [SerializeField]public TriggerQuestCondition triggerQuestCondition;
-    
+    [SerializeField]public QuestAction beginningQuestAction;
+    [SerializeField]public QuestDialogue descriptionDialogue;
+    [SerializeField]public QuestDialogue endingDialogue;
+    [SerializeField]public QuestAction endingQuestAction;
+
+    private void Start()
+    {
+        
+    }
 }
