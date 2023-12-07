40ce3b95dfb2d67b1667fc89360ab03c49d0da45
Peng Guo <errrrrricg@gmail.com>
2023-12-07 13:45:50 -0500

    connect fog with quest 1

--- Assets/Prefabs/Opening Fog/OpeningFogController.cs
+++ Assets/Prefabs/Opening Fog/OpeningFogController.cs
@@ -12,18 +12,39 @@ public class OpeningFogController : MonoBehaviour
     void Start()
     {
         fogVFX = GetComponent<VisualEffect>();
+        if (index == 2)
+        {
+            SpiritPoint.i.GetPlayerStat().SubscribeStatChange(UpdateInnerCircle);
+        }
     }
 
     void Update()
     {
         if ((Input.GetKeyDown(KeyCode.Alpha1) && index == 1) || (Input.GetKeyDown(KeyCode.Alpha2) && index == 2))
         {
-            Debug.Log("Stop Fog");
-
-            fogVFX.Stop();
-            fogVFX.playRate = 2.0f;
-
-            Destroy(gameObject, 4.0f);
+            DeleteClouds();
         }
+    }
+
+    void DeleteClouds()
+    {
+        fogVFX.Stop();
+        fogVFX.playRate = 2.0f;
+        Destroy(gameObject, 4.0f);
+    }
+
+    void UpdateInnerCircle(int currentAmount)
+    {
+        if(index != 2) return;
+        if(currentAmount > 8) return;
+        if (currentAmount == 8)
+        {
+            DeleteClouds();
+            return;
+        }
+        float currentX = fogVFX.GetFloat("SDFSizeX");
+        float currentZ = fogVFX.GetFloat("SDFSizeZ");
+        fogVFX.SetFloat("SDFSizeX",currentX * 1.1f);
+        fogVFX.SetFloat("SDFSizeZ",currentZ * 1.1f);
     }
 }
