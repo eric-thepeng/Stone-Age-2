fe4892ddf6c68ab3ff6f052f4a15978fdd49b8e7
Bowen <1758364810@qq.com>
2023-12-09 14:51:44 -0500

    somehow changed mouse interactable icon

--- Assets/Scripts/World Interaction/Interaction System/WorldInteractable.cs
+++ Assets/Scripts/World Interaction/Interaction System/WorldInteractable.cs
@@ -172,6 +172,7 @@ public class WorldInteractable : MonoBehaviour
     {
         if(!CanInteract()) return;
         currentInteraction?.ResetProgress(true);
+        currentInteraction?.DisplayUI(false); // changed after comments
     }
 
     protected virtual void WhileMousePress()
@@ -187,6 +188,7 @@ public class WorldInteractable : MonoBehaviour
         if (currentInteraction != null && currentInteraction.IsCompleted())
         {
             currentInteraction = null;
+
         }
     }
 
