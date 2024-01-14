b50be534b8fc32598ace15dfa9988e6e5dfcc7a8
bellinimoon <98369237+bellinimoon@users.noreply.github.com>
2024-01-14 12:55:54 +0000

    tooltip update
    need to add different templates

--- Assets/Scripts/UI/TooltipManager.cs
+++ Assets/Scripts/UI/TooltipManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class TooltipManager : MonoBehaviour
 {
@@ -21,29 +23,60 @@ public class TooltipManager : MonoBehaviour
     {
         ItemScriptableObject iso;
         public GameObject displayTip;
-
+        public float tipWidth;
+        public float tipHeight;
+        private float textHeight;
         public Tooltip(ItemScriptableObject newIso, GameObject tip)
         {
             iso = newIso;
             displayTip = tip;
-        }
+            tip.transform.Find("Text").transform.Find("Title").GetComponent<TextMeshPro>().text = iso.tetrisHoverName;
+            tip.transform.Find("Text").transform.Find("Description").GetComponent<TextMeshPro>().text = iso.tetrisDescription;
+            LayoutRebuilder.ForceRebuildLayoutImmediate(tip.transform.Find("Text").transform.Find("Description").GetComponent<RectTransform>());
+            LayoutRebuilder.ForceRebuildLayoutImmediate(tip.transform.Find("Text").transform.Find("Title").GetComponent<RectTransform>());
+            textHeight = tip.transform.Find("Text").transform.Find("Title").GetComponent<RectTransform>().rect.height + tip.transform.Find("Text").transform.Find("Description").GetComponent<RectTransform>().rect.height;
 
+            tipWidth = tip.transform.Find("Background").GetComponent<Renderer>().bounds.size.x;
+            tipHeight = tip.transform.Find("Background").GetComponent<Renderer>().bounds.size.y;
+
+
+            Vector3 targetScale = tip.transform.Find("Background").transform.localScale;
+            targetScale.y = (textHeight / tipHeight) * targetScale.y;
+            tip.transform.Find("Background").transform.localScale = targetScale;
+
+            print("tip" + tipHeight + "    TEXT" + textHeight);
+        }
         public void changePosition(Vector3 mousePosition)
         {
-            displayTip.transform.position = mousePosition;
+            if(displayTip != null)
+            {
+                displayTip.transform.position = mousePosition;
+            }
+
         }
+
         public void DestroyDisplay()
         {
             Destroy(displayTip);
         }
-
     }
+
     [SerializeField] GameObject tipTemplate;
+    [SerializeField] float offset;
     private Vector3 mousePos;
-    private Tooltip tip;
+    Tooltip tip;
     private Vector3 lastMousePosition;
     private GameObject newDisplayTip;
-    public ItemScriptableObject ISO;
+    //public ItemScriptableObject ISO;
+
+    public enum MouseArea
+    {
+        TOPLEFT,
+        TOPRIGHT,
+        BOTTOMLEFT,
+        BOTTOMRIGHT
+    }
+    
     public void ShowTip(ItemScriptableObject iso)
     {
         newDisplayTip = Instantiate(tipTemplate, this.transform);
@@ -51,34 +84,99 @@ public class TooltipManager : MonoBehaviour
         tip = new Tooltip(iso, newDisplayTip);
     }
 
-    public void updateTipPosition(Vector3 mousePos)
+
+    public void UpdateTipPosition(Vector3 mousePos, MouseArea mouseArea)
     {
-        tip.changePosition(mousePos);
+        float _width = tip.tipWidth;
+        float _height = tip.tipHeight;
+        Vector3 newPosition = mousePos;
+        switch (mouseArea)
+        {
+            case MouseArea.TOPLEFT:
+                newPosition = new Vector3(mousePos.x + offset, mousePos.y - offset, mousePos.z);
+                break;
+            case MouseArea.TOPRIGHT:
+                newPosition = new Vector3(mousePos.x - offset, mousePos.y - offset, mousePos.z);
+                break;
+            case MouseArea.BOTTOMLEFT:
+                newPosition = new Vector3(mousePos.x + offset, mousePos.y + offset, mousePos.z);
+                break;
+            case MouseArea.BOTTOMRIGHT:
+                newPosition = new Vector3(mousePos.x - offset, mousePos.y + offset, mousePos.z);
+                break;
+            default:
+                newPosition = new Vector3(mousePos.x - offset, mousePos.y - offset, mousePos.z);
+                break;
+        }
+        if(tip!= null)
+        {
+            tip.changePosition(newPosition);
+        }   
+       
     }
 
     public void DestroyTip()
     {
         tip.DestroyDisplay();
     }
+
     void Start()
     {
-        ShowTip(ISO);
+        //ShowTip(ISO);
         lastMousePosition = Input.mousePosition;
     }
+
     void Update()
     {
+        int screenHeight = Screen.height;
+        int screenWidth = Screen.width;
         RaycastHit hit;
         Vector3 _mousePosition = Input.mousePosition;
+        MouseArea currentMouseArea = MouseArea.TOPRIGHT;
         if (Physics.Raycast(Camera.main.ScreenPointToRay(_mousePosition), out hit))
         {
             mousePos = hit.point;
         }
 
+        //change of mousse position
         if (_mousePosition != lastMousePosition)
         {
-            updateTipPosition(mousePos);
+            if (_mousePosition.y > screenHeight / 2)
+            {
+                if (_mousePosition.x < screenWidth / 2)
+                {
+                    currentMouseArea = MouseArea.TOPLEFT;
+                    //Debug.Log("Mouse is in the top left.");
+                }
+                else
+                {
+                    currentMouseArea = MouseArea.TOPRIGHT;
+                    //Debug.Log("Mouse is in the top right.");
+                }
+            }
+            else
+            {
+                if (_mousePosition.x < screenWidth / 2)
+                {
+                    currentMouseArea = MouseArea.BOTTOMLEFT;
+                    //Debug.Log("Mouse is in the bottom left.");
+                }
+                else
+                {
+                    currentMouseArea = MouseArea.BOTTOMRIGHT;
+                    //Debug.Log("Mouse is in the bottom right.");
+                }
+
+            }
+
+            if(tip != null)
+            {
+                UpdateTipPosition(mousePos, currentMouseArea);
+            }
+            //UpdateTipPosition(mousePos, currentMouseArea);
             lastMousePosition = _mousePosition;
         }
+    
 
     }
 }
