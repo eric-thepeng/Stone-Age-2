ca542fdeca9d977cc9de4f52017e734997f2148a
Bowen <1758364810@qq.com>
2024-01-22 01:14:43 -0500

    fixed grid tile water layer

--- Assets/Plugins/HypertonicGames/GridPlacement/Runtime/Scripts/GridDisplayManager.cs
+++ Assets/Plugins/HypertonicGames/GridPlacement/Runtime/Scripts/GridDisplayManager.cs
@@ -150,7 +150,7 @@ namespace Hypertonic.GridPlacement
             Tilemap RegionPresetTilemap = gridSettings.RegionTypePresetTilemap.GetComponent<Tilemap>();
             
             TilemapRenderer tilemapRenderer = tilemapGameObject.AddComponent<TilemapRenderer>();
-            tilemapRenderer.sortingOrder = -10;
+            tilemapRenderer.sortingOrder = 0;
 
             tilemapGameObject.transform.localScale = new Vector3(2, 2, 2);
             tilemapGameObject.transform.rotation = Quaternion.Euler(90,0,0);
