aab7cd5a454c0bdde7de130cafed0ddfe6ea8c56
Peng Guo <errrrrricg@gmail.com>
2024-02-27 22:00:55 -0500

    completely finished - blueprint implementation 1,2,3

--- Assets/Scripts/World Interaction/Map Interactable/MIS_FindBlueprint.cs
+++ Assets/Scripts/World Interaction/Map Interactable/MIS_FindBlueprint.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class MIS_FindBlueprint : MonoBehaviour, ISerialEffect
 {
     [SerializeField] private List<ItemCraftScriptableObject> blueprintsToObtain;
-    [SerializeField] private int obtainAtLevelState = 1;
+    [SerializeField] private int obtainAtLevelState = 2;
     [SerializeField] private SO_SerialEffectIdentifier serialEffectIdentifier;
 
+    private LevelUp myLevelUp;
 
     private void Awake()
     {
+        myLevelUp = GetComponent<LevelUp>();
         SetUpSerialEffectIdentifier();
-        GetComponent<LevelUp>().GetCurrentStatePlayerStat().SubscribeStatChange(CheckToRecruit);
+        myLevelUp.GetCurrentStatePlayerStat().SubscribeStatChange(CheckToRecruit);
     }
 
     private void CheckToRecruit(int level)
@@ -39,7 +42,8 @@ public class MIS_FindBlueprint : MonoBehaviour, ISerialEffect
 
     public void ReceiveSerialEffect(SO_SerialEffectIdentifier origionSEI)
     {
-        //unlockToState_1_Locked();
+        if(myLevelUp.GetCurrentState()!=0) return;
+        if(!myLevelUp.UnlockToNextState()) return;
         UI_ExploreSpotsConnection.i.UnlockLine(origionSEI, mySEI);
     }
 
