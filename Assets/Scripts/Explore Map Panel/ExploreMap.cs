c4041b61d448595e6fd9d7bdee632ddd71eeabb0
Peng Guo <errrrrricg@gmail.com>
2024-03-02 02:01:12 -0500

    new scroll art + clamp left right

--- Assets/Scripts/Explore Map Panel/ExploreMap.cs
+++ Assets/Scripts/Explore Map Panel/ExploreMap.cs
@@ -1,5 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using Sirenix.Utilities;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ExploreMap : MonoBehaviour
@@ -7,6 +10,9 @@ public class ExploreMap : MonoBehaviour
     float momentum = 0f;
     [SerializeField]private float moveSpeed = 1f;
     Vector2 direction = new Vector2(0,0);
+
+    private float mapXMax = -12.5f;
+    private float mapXMin = -31f;
 
     private void Update()
     {
@@ -28,7 +34,9 @@ public class ExploreMap : MonoBehaviour
             momentum = 1;
         }
         transform.localPosition += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * moveSpeed * momentum;
+        transform.localPosition = new Vector3(Mathf.Clamp(transform.localPosition.x, mapXMin, mapXMax),
+                transform.localPosition.y,
+                transform.localPosition.z);
         //if(restrainedCamera) transform.position = new Vector3(Mathf.Clamp(transform.position.x, cameraXMin, cameraXMax), transform.position.y, Mathf.Clamp(transform.position.z, cameraZMin, cameraZMax));
-
     }
 }
