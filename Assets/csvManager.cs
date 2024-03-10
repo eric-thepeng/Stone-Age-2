a133fd665b98d2cfa48bc23b501e040f0d424226
bellinimoon <98369237+bellinimoon@users.noreply.github.com>
2024-03-09 21:34:09 -0500

    quick save

--- Assets/csvManager.cs
+++ Assets/csvManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Assets.SimpleLocalization.Scripts;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
 
 public class csvManager : MonoBehaviour
 {
@@ -9,6 +11,41 @@ public class csvManager : MonoBehaviour
     {
         LocalizationManager.Read();
         LocalizationManager.Language = "BASIC DESCRIPTION";
-        Debug.Log("okk" + LocalizationManager.Localize("Egg Fried Rice"));
+        Debug.Log("basic: " + LocalizationManager.Localize("Egg Fried Rice"));
     }
+
+    [MenuItem("Assets/Update ScriptableObjects")]
+    static void UpdateScriptableObjects()
+    {
+        //string relativePath = Resources.Load<Item>("Items/MyItem");
+        Dictionary<string, string> data = ReadCSV("./ISO_description.txt");
+        foreach (KeyValuePair<string, string> pair in data)
+        {
+            string path = "Assets/ScriptableObjects/CraftingSystem/ItemScriptableObject" + pair.Key + ".asset";
+            ItemScriptableObject obj = AssetDatabase.LoadAssetAtPath<ItemScriptableObject>(path);
+
+            if (obj != null)
+            {
+                obj.tetrisDescription = pair.Value;
+                EditorUtility.SetDirty(obj);
+            }
+        }
+
+        AssetDatabase.SaveAssets();
+    }
+
+    public static Dictionary<string, string> ReadCSV(string file)
+    {
+        var result = new Dictionary<string, string>();
+        string[] lines = File.ReadAllLines(file);
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            string[] col = lines[i].Split(';');
+            result.Add(col[0], col[1]);
+        }
+
+        return result;
+    }
+
 }
