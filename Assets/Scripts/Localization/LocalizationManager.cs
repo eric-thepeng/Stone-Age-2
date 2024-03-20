e94698b6d35aa618d919650c1eadc5f25354d7c1
bellinimoon <98369237+bellinimoon@users.noreply.github.com>
2024-03-20 10:39:39 -0700

    builldable

--- Assets/Scripts/Localization/LocalizationManager.cs
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.UIElements;
+//using UnityEditor.UIElements;
 using UnityEngine;
 
 public class LocalizationManager : MonoBehaviour
